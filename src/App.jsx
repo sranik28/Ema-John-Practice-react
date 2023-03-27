e77913c2839ac88a99a241b74e928081a0fc7a81
sranik28~ <anikshariarrahman@gmail.com>
2023-03-27 13:04:02 +0600

    header added

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './componentes/Header/Header'
+import Cards from './componentes/Cards/Cards'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <Header></Header>
+      {/* <Cards></Cards> */}
+    </div>
+  )
+}
+
+export default App
