042b645934373ae05d7b748161497c3a44128455
sranik28~ <anikshariarrahman@gmail.com>
2023-03-29 03:45:44 +0600

    show data from card

--- src/componentes/Cards/Cards.jsx
+++ src/componentes/Cards/Cards.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { addToDb } from '../../../utilities/fakedb';
+import { addToDb, getShoppingCart } from '../../../utilities/fakedb';
 import Order from '../OrderSummary/Order';
 import Product from '../product/product';
 const Cards = () => {
     const [card, setCard] = useState([]);
 
-    const [shop, setShop] = useState([]); 
+    const [shop, setShop] = useState([]);
 
     useEffect(() => {
         const loadDAta = async () => {
@@ -16,9 +16,40 @@ const Cards = () => {
         loadDAta()
     }, [])
 
+    useEffect(() => {
+        const strodeCard = getShoppingCart();
+        const addCard = [];
+
+        for (const id in strodeCard) {
+            const savedProduct = card.find(product => product.id === id);
+            if (savedProduct) {
+
+                // console.log(savedProduct)
+                const quantity = strodeCard[id];
+                savedProduct.quantity = quantity
+                // console.log(savedProduct)
+                addCard.push(savedProduct)
+            }
+        }
+        setShop(addCard)
+    }, [card])
+
     const handleAddToCard = (singleData) => {
+
+        let newProduct = [];
+
         // shop.push(singleData)
-        const newProduct = [...shop, singleData];
+        // const newProduct = [...shop, singleData];
+        const exists = card.find(pd => pd.id === singleData.id);
+        if (!exists) {
+            singleData.quantity = 1;
+            newProduct = [...shop, singleData]
+        }
+        else {
+            exists.quantity = exists.quantity + 1;
+            const remaining = shop.filter(pd => pd.id !== singleData.id);
+            newProduct = [...remaining, exists]
+        }
         setShop(newProduct);
         addToDb(singleData.id)
     }
@@ -30,7 +61,7 @@ const Cards = () => {
                     card.map(singleData => <Product singleData={singleData} key={singleData.id} handleAddToCard={handleAddToCard} ></Product>)
                 }
             </div>
-            <div className='w-[300px]    bg-orange-200 '>
+            <div className='w-[300px] h-96   bg-orange-200 '>
                 <Order Order={shop}></Order>
             </div>
         </div>
--- src/componentes/OrderSummary/Order.jsx
+++ src/componentes/OrderSummary/Order.jsx
@@ -6,9 +6,14 @@ const Order = ({ Order }) => {
 
     let totalPrice = 0;
     let totalShipping = 0;
+    let quantity = 0;
     for (const product of Order) {
-        totalPrice = totalPrice + product.price;
+
+        product.quantity = product.quantity || 1;
+
+        totalPrice = totalPrice + product.price * product.quantity;
         totalShipping = totalShipping + product.shipping
+        quantity = quantity + product.quantity;
     }
 
     const tex = totalPrice * 5 / 100;
@@ -19,7 +24,7 @@ const Order = ({ Order }) => {
     return (
         <div>
             <h1 className=' font-bold text-center text-3xl my-8'>Order summary </h1>
-            <p className='px-5 my-3'>Selected Items:{Order.length}</p>
+            <p className='px-5 my-3'>Selected Items:{quantity}</p>
             <p className='px-5 my-3'>Total Price:$ {totalPrice}</p>
             <p className='px-5 my-3'>Total Shipping:$ {totalShipping}</p>
             <p className='px-5 my-3'>Tax:& {tex.toFixed(2)}</p>
